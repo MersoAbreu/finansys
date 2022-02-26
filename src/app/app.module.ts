f48e8afd643b2256b49f72aa409a7cac21eed63d
Emerson de Abreu <mersoabreu@gmail.com>
2022-02-26 12:18:24 -0300

    Criação de telas iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesModule } from './pages/categories/categories.module';
+import { NavbarComponent } from './static/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
